da988f487a994f28315db664839e35af9c60ad53
RobLBaker <rlbaker5@gmail.com>
2025-12-15 22:49:28 +0000

    update side bar text

--- app.R
+++ app.R
@@ -11,7 +11,66 @@ ui <- page_sidebar(
   # ---- Sidebar ----
   sidebar = sidebar(
     width = "25%",
-    title = "About this app"
+    title = "About this app",
+    accordion(
+      accordion_panel("About",
+        helpText("bReezEML is a Tool for creating",
+             "Ecological Metadata Language metadata. bReezEML servers as a ",
+             "wrapper for the series of packages and functions in the",
+             a("NPSdataverse R packages",
+               href = "https://nationalparkservice.github.io/NPSdataverse/",
+               target = "_blank"),
+             ". bReezEML is maintained by the National Park Service and is ",
+             "specifically designed to help create data packages to be ",
+             "uploaded to the NPS designated science repository,",
+             a("DataStore",
+               href="https://irma.nps.gov/DataStore/",
+               target = "_blank"),
+             ".")
+      ),
+      accordion_panel("Cite bReezEML",
+        helpText("Please cite the Journal of Open Source Software",
+             a("publication",
+               href = "https://doi.org/10.21105/joss.08066",
+               target = "_blank"),
+             " associated with the NPSdataverse: ",
+             br(),
+             br(),
+             "Baker et al. (2025). NPSdataverse: a suite of R packages for ",
+             "data processing, authoring Ecological Metadata Language ",
+             "metadata, checking data-metadata congruence, and ",
+             "accessing data. Journal of Open Source Software, 10(109),",
+             " 8066, ",
+             a("https://doi.org/10.21105/joss.08066",
+               href = "https://doi.org/10.21105/joss.08066",
+               target = "_blank")
+             )
+      ),
+      accordion_panel("Help",
+                      helpText("Contacts:",
+                               br(),
+                               "Maintainers: ",
+                               a("sarah_wright@nps.gov",
+                                 href = "mailto:sarah_wright@nps.gov"),
+                               br(),
+                               a("robert_baker@nps.gov",
+                                 href = "mailto:robert_baker@nps.gov")
+                               )
+                      )
+      ),
+      accordion_panel("Issues",
+                    helpText("Please use github for all ",
+                             a("issues",
+                               href = "https://github.com/nationalparkservice/shinyEML/issues"),
+                             "."
+                             )
+                    ),
+      accordion_panel("Source Code",
+                    helpText("Source code can be found on ",
+                             a("GitHub.com",
+                               href = "https://github.com/nationalparkservice/shinyEML"),
+                             ".")
+    )
   ),
   # --- End sidebar ---
   
